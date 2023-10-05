abda558d0a87244b91f4f0357d39c47a91ff074b
Whales <Whales.w.Jin@gmail.com>
2023-10-05 21:53:15 +0900

    feat: 프로젝트 파일 생성

---
+++ 2048/2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Whales on 10/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
